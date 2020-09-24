4167b93209508c4a679bb777bb889cb5e49f0e55
thackett117 <thackett117@gmail.com>
2020-09-24 11:37:36 -0500

    making the sentence display and move the highlight

--- app.js
+++ app.js
@@ -1,32 +1,50 @@
-$('document').ready(function () {
 
-    //variables and arrays
-    let sentences = ['ten ate neite ate nee enet ite ate inet ent eate', 'Too ato too nOt enot one totA not anot tOO aNot', 'oat itain oat tain nate eate tea anne inant nean', 'itant eate anot eat nato inate eat anot tain eat', 'nee ene ate ite tent tiet ent ine ene ete ene ate'];
+//variables and arrays
+let sentences = [
+    'ten ate neite ate nee enet ite ate inet ent eate',
+    'Too ato too nOt enot one totA not anot tOO aNot',
+    'oat itain oat tain nate eate tea anne inant nean',
+    'itant eate anot eat nato inate eat anot tain eat',
+    'nee ene ate ite tent tiet ent ine ene ete ene ate'
+];
 
-    //hide upper case keyboard on load
-    $('#keyboard-upper-container').hide();
+let sentenceCount = 0;
+let $sentences = $('#sentence');
+let $yellowBlock = $('#yellow-block');
+let $targetLetter = $('#target-letter');
 
-
-    //toggle uppercase keyboard
-    $(document).keydown(function (e) {
-        if (e.keyCode === 16) {
-            $('#keyboard-lower-container').hide();
-            $('#keyboard-upper-container').show();
-        }
-    });
-    $(document).keyup(function (e) {
-        $('.highlight').removeClass('highlight');
-        if (e.keyCode === 16) {
-
-            $('#keyboard-upper-container').hide();
-            $('#keyboard-lower-container').show();
-        }
-    })
+//hide upper case keyboard on load
+$('#keyboard-upper-container').hide();
 
 
-    //highlight keys that are pressed
-    $(document).keypress(function (e) {
-        $('#' + e.keyCode).addClass('highlight');
-    })
+//toggle uppercase keyboard
+$(document).keydown(function (e) {
+    if (e.keyCode === 16) {
+        $('#keyboard-lower-container').hide();
+        $('#keyboard-upper-container').show();
+    }
+});
+$(document).keyup(function (e) {
+    $('.highlight').removeClass('highlight');
+    if (e.keyCode === 16) {
 
+        $('#keyboard-upper-container').hide();
+        $('#keyboard-lower-container').show();
+    }
 })
+
+
+//highlight keys that are pressed
+$(document).keypress(function (e) {
+    $('#' + e.keyCode).addClass('highlight');
+    $yellowBlock.css('left', "+=20px");
+})
+
+
+//display sentences at top
+$('<h2>' + sentences[sentenceCount] + '</h2>').appendTo($sentences)
+
+
+//display target letter
+$('<h3>' + 't' + '</h3>').appendTo($targetLetter);
+
